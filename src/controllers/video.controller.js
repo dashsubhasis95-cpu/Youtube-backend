ecbe188cd458c98acfbe66c36149332fddd07336
Subhasis Dash <dashsubhasis95@gmail.com>
2026-03-04 22:22:32 +0530

    add publishAVideo function to handle video publishing; update isPublished default value to false

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -23,3 +23,16 @@ const getAllVideos = asyncHandler(async (req, res) => {
     new ApiResponse(200, videos, "Videos fetched successfully")
    )
 })
+
+const publishAVideo = asyncHandler(async (req, res) => {
+    const {videoId}= req.params
+    const { title, description, thumbnail, videoUrl } = req.body
+    const video = await Video.findByIdAndUpdate(
+        videoId,
+        { title, description, thumbnail, videoUrl, isPublished: true },
+        { new: true }
+    )
+    res.status(200).json(
+        new ApiResponse(200, video, "Video published successfully")
+    )
+})
