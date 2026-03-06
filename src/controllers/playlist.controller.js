470355896b41c6455763804a4dc03e8b0b67ca7c
Subhasis Dash <dashsubhasis95@gmail.com>
2026-03-06 22:57:20 +0530

    feat: add getUserPlaylists function to retrieve playlists for a specific user

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -19,3 +19,60 @@ const createPlaylist = asyncHandler(async (req, res) => {
     )
 
 })
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+    const {userId} = req.params
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        throw new ApiError(400, "Invalid user id")
+    }
+
+    const playlists = await Playlist.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localfield: "videos",
+                foreignField: "_id",
+                as: "videos"
+            }
+        },
+        {
+            $addFields: {
+                videoCount: {
+                    $size: "$videos"
+                },
+                thumbnail: {
+                    $slice: ["$videos.thumbnail", 1]
+                }
+            }
+        },
+        {
+            $project: {
+                name: 1,
+                description: 1,
+                videoCount: 1,
+                thumbnail: 1,
+                owner: 1,
+                createdAt: 1,
+            }
+        },
+        {
+            $sort: { createdAt: -1 }
+        }
+    ])
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            playlists,
+            "User playlists fetched successfully"
+        )
+    )
+
+
+
+})
