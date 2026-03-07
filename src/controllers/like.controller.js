f4585c3fde2c51cbc5c0f93c27f6c661eb41fa63
Subhasis Dash <dashsubhasis95@gmail.com>
2026-03-07 23:51:17 +0530

    : refactor toggleVideoLike function to improve like handling and response messages

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,20 +1,40 @@
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
-    const video = await Video.findById(videoId)
 
-    if(!mongoose.Types.ObjectId.isValid(videoId)){
+    const { videoId } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
         throw new ApiError(400, "Invalid video id")
     }
+
+    const video = await Video.findById(videoId)
 
     if (!video) {
         throw new ApiError(404, "Video not found")
     }
 
-    const like = await Like.findByIdAndUpdate(
-        { video: videoId, likedBy: req.user._id },
-        { $set: { isLiked: !like.isLiked } },
-        { new: true, upsert: true }
-    )
-})
+    const existingLike = await Like.findOne({
+        video: videoId,
+        likedBy: req.user._id
+    })
 
+    if (existingLike) {
+
+        await Like.findByIdAndDelete(existingLike._id)
+
+        return res.status(200).json(
+            new ApiResponse(200, {}, "Video unliked")
+        )
+
+    } else {
+
+        await Like.create({
+            video: videoId,
+            likedBy: req.user._id
+        })
+
+        return res.status(200).json(
+            new ApiResponse(200, {}, "Video liked")
+        )
+    }
+
+})
