d9755b08b16760d99af4e60470e21faeb3338764
Subhasis Dash <dashsubhasis95@gmail.com>
2026-03-10 23:58:21 +0530

    feat: add toggleCommentLike function to handle comment like toggling

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -39,4 +39,9 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 
 })
 
-const 
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const {commentId} = req.params
+    //TODO: toggle like on comment
+    
+
+})
