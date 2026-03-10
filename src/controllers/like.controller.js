076846e405ce966ad25998417eaa90c4c7e7bd89
Subhasis Dash <dashsubhasis95@gmail.com>
2026-03-11 00:00:03 +0530

    add getLikedVideos function to retrieve all liked videos

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -52,3 +52,7 @@ const toggleTweetLike = asyncHandler(async (req, res) => {
     //TODO: toggle like on tweet
 }
 )
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+    //TODO: get all liked videos
+})
