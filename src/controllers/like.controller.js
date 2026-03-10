b7b0514b2671189b6212bedaa9ae1df92491308b
Subhasis Dash <dashsubhasis95@gmail.com>
2026-03-11 00:25:22 +0530

    enhance toggleCommentLike and toggleTweetLike functions for improved like handling and validation

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -40,14 +40,16 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
-    if(!mongoose.Types.ObjectId.isValid(commentId)){
+
+    const { commentId } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(commentId)) {
         throw new ApiError(400, "Invalid comment id")
     }
-    const comment = await Comment.findById(commentId)
 
-    if(!comment){
+    const commentExists = await Comment.exists({ _id: commentId })
+
+    if (!commentExists) {
         throw new ApiError(404, "Comment not found")
     }
 
@@ -55,28 +57,87 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
         comment: commentId,
         likedBy: req.user._id
     })
-    if(existingLike){
-        await Like.findByIdAndDelete(existingLike._id)
-        return res.status(200).json(
-            new ApiResponse(200, {}, "Comment unliked")
-        )
+
+    let isLiked
+
+    if (existingLike) {
+
+        await Like.deleteOne({ _id: existingLike._id })
+
+        isLiked = false
+
     } else {
+
         await Like.create({
             comment: commentId,
             likedBy: req.user._id
         })
-        return res.status(200).json(
-            new ApiResponse(200, {}, "Comment liked")
-        )
+
+        isLiked = true
     }
 
+    const likeCount = await Like.countDocuments({
+        comment: commentId
+    })
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {
+                isLiked,
+                likeCount
+            },
+            "Comment like toggled successfully"
+        )
+    )
 
 })
 
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
+    
+    const { tweetid } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(tweetid)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+    const tweetExists = await Tweet.exists({ _id: tweetid })
+
+    if (!tweetExists) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    const existingLike = await Like.findOne({
+        tweet: tweetid,
+        likedBy: req.user._id
+    })
+    let isLiked
+
+    if (existingLike) {
+        await Like.deleteOne({ _id: existingLike._id })
+        isLiked = false
+    } else {
+        await Like.create({
+            tweet: tweetid,
+            likedBy: req.user._id
+        })
+        isLiked = true
+    }
+
+    const likeCount = await Like.countDocuments({
+        tweet: tweetid
+    })
+
+    return res.status(200).json(
+        new ApiResponse(200,
+            {
+                isLiked,
+                likeCount
+            },
+                "Tweet like toggled successfully"
+        )
+    )
+
 }
 )
 
