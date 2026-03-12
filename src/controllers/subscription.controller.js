f1e6226fe7e499f0df03d8a625260aa16ca7a459
Subhasis Dash <dashsubhasis95@gmail.com>
2026-03-12 23:45:07 +0530

    refactor toggleSubscription to improve readability and remove unnecessary checks

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,18 +1,11 @@
 import e = require("express")
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+
+    const { channelId } = req.params
 
     if (!mongoose.Types.ObjectId.isValid(channelId)) {
         throw new ApiError(400, "Invalid channel id")
-    }
-
-    const Subscriptionexists = await Subscription.exists({
-        subscriber: req.user._id,
-        channel: channelId
-    })
-    if(!Subscriptionexists){
-        throw new ApiError(404, "Channel not found")
     }
 
     const existingSubscription = await Subscription.findOne({
@@ -23,14 +16,19 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     if (existingSubscription) {
 
         await Subscription.findByIdAndDelete(existingSubscription._id)
-    }
-    else {
-        await Subscription.create({
-            subscriber: req.user._id,
-            channel: channelId
-        })
+
+        return res.status(200).json(
+            new ApiResponse(200, {}, "Unsubscribed from channel")
+        )
     }
 
+    await Subscription.create({
+        subscriber: req.user._id,
+        channel: channelId
+    })
+
     return res.status(200).json(
+        new ApiResponse(200, {}, "Subscribed to channel")
+    )
 
 })
