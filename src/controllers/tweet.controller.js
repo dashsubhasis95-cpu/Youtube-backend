7665899996558c8e17f9cb96daa65d8b3f64c503
Subhasis Dash <dashsubhasis95@gmail.com>
2026-03-13 23:14:27 +0530

    update in tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -12,3 +12,15 @@ const createTweet = asyncHandler(async (req, res) => {
 
     return res.status(201).json(new ApiResponse(201, tweet, "Tweet created successfully"))
 })
+
+const getUserTweets = asyncHandler(async (req, res) => {
+    // TODO: get user tweets
+    const { userId } = req.params
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        throw new ApiError(400, "Invalid user id")
+     }
+
+    const tweets = await Tweet.find({ owner: userId }).sort({ createdAt: -1 })
+
+    return res.status(200).json(new ApiResponse(200, tweets, "User tweets fetched successfully"))
+})
